819aea621ec6784acc98a1db45c31f90a1870fd9
Omoegbeleghan <favourjromoegbeleghan@gmail.com>
2025-06-28 01:43:04 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,8 +1,8 @@
 const settings = {
   packname: 'Knight Bot',
   author: '‎',
-  botName: "Favour O",
-  botOwner: 'Professor', // Your name
+  botName: "Knight Bot",
+  botOwner: 'Favour O, // Your name
   ownerNumber: '2347059804966', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
